5306c4ce74640d7f577d03ddb3abb8b9ce995b72
Vinicius Souza <vinicius.fernando.souza@gmail.com>
2021-08-31 13:39:19 -0300

    refact(project): :whale: refatora modulos para trabalharem com Docker
    sign: Vinicius Souza <vinicius.fernando.souza@gmail.com>

--- chatbot/main.py
+++ chatbot/main.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # nopep8
 from chatbot.support import generate_intent_nlu
-from chatbot.support import set_telegram_config
+# from chatbot.support import set_telegram_config
 
 generate_intent_nlu.main()
-set_telegram_config.main()
+# set_telegram_config.main()
