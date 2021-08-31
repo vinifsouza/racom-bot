e9d1e1d00509c4d463780b6d70b86c4f2801f5f4
Vinicius Souza <vinicius.fernando.souza@gmail.com>
2021-08-31 18:50:18 -0300

    fix(chatbot): :bug: :whale: corrige config para import relativo dentro do container
    sign: Vinicius Souza <vinicius.fernando.souza@gmail.com>

--- chatbot/config.py
+++ chatbot/config.py
@@ -1,13 +1,17 @@
 import os
 
-API_PORT = os.environ.get('API_PORT') or '4444'
-API_HOST = os.environ.get('API_HOST') or 'http://localhost'
-RANCOM_API_URL = f'http://{API_HOST}:{API_PORT}'
+API_PORT = os.environ.get("API_PORT") or "4444"
+API_HOST = os.environ.get("API_HOST") or "http://localhost"
+RANCOM_API_URL = f"http://{API_HOST}:{API_PORT}"
 
-NGROK_HOST = os.environ.get('NGROK_HOST') or 'http://localhost'
-NGROK_PORT = os.environ.get('NGROK_PORT') or '4040'
-NGROK_API_URL = f'http://localhost:{NGROK_PORT}'
+NGROK_HOST = os.environ.get("NGROK_HOST") or "localhost"
+NGROK_PORT = os.environ.get("NGROK_PORT") or "4040"
+NGROK_API_URL = f"http://{NGROK_HOST}:{NGROK_PORT}"
 
-CHATBOT_NLU_YML_PATH = './data/nlu.yml'
-CHATBOT_DOMAIN_YML_PATH = 'domain.yml'
-CHATBOT_CREDENTIALS_YML_PATH = 'credentials.yml'
+IS_DOCKER = os.environ.get("IS_DOCKER") or False
+
+PATH = "/chatbot/" if IS_DOCKER else "/"
+
+CHATBOT_NLU_YML_PATH = f"{PATH}data/nlu.yml"
+CHATBOT_DOMAIN_YML_PATH = f"{PATH}domain.yml"
+CHATBOT_CREDENTIALS_YML_PATH = f"{PATH}credentials.yml"
